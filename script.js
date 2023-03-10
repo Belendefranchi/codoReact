18604ee42795842305ed99c7f121d7eaedad468a
Belen De Franchi <belendefranchi@gmail.com>
2023-03-10 19:37:10 -0300

    empiezo card ID

---
+++ script.js
@@ -0,0 +1,21 @@
+const btn = document.getElementById('btn');
+const card = document.getElementsByClassName('card');
+
+/* btn.addEventListener('click', () => {
+		card.classList.add('visible');
+}); */
+
+/* btn.addEventListener('click', function()  {
+    let elements = document.getElementsByClassName('card');
+    let screenSize = window.innerHeight;
+    
+    for(var i = 0; i < elements.length; i++) {
+			let element = elements[i];
+
+			if(element.getBoundingClientRect().top < screenSize) {
+				element.classList.add('visible');
+			} else {
+				element.classList.remove('visible');
+			}
+		}
+  }) */
